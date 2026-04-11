9010126d3e84340f4b38cc28934ea8137b1427ec
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-04-11 20:35:05 +0200

    Clean project structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { initGallery } from "./src/gallery/index.js";
-import { initEntryGate } from "./src/puzzle/entryGate.js";
-import { getAvailablePuzzleImages } from "./src/puzzle/imageManifest.js";
+import "./styles.css";
+
+import { initGallery } from "./gallery/index.js";
+import { initEntryGate } from "./puzzle/entryGate.js";
+import { getAvailablePuzzleImages } from "./puzzle/imageManifest.js";
 
 if (typeof window !== "undefined" && typeof document !== "undefined") {
   const images = getAvailablePuzzleImages();
